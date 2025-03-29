e5e3d1871942dd32a8962c9bbff3cb0f6381e947
floresrf <areneflores10@gmail.com>
2025-03-29 14:48:54 -0700

    correccion de la tabla

--- src/app/modules/menu/chart/chart.component.ts
+++ src/app/modules/menu/chart/chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Chart, registerables } from 'chart.js';
+import { Chart, Colors, registerables } from 'chart.js';
 Chart.register(...registerables);
 
 @Component({
@@ -10,29 +10,43 @@ Chart.register(...registerables);
   styleUrl: './chart.component.css'
 })
 export class ChartComponent implements OnInit{
-  public config: any = {
+  public config: any = {   
+
+        //'rgb(8, 51, 129)',
+        //'rgb(20, 192, 155)',
+        //'rgb(19, 138, 35)',
+        //'rgb(83, 226, 47)',
+        //'rgb(245, 255, 105)',
+        //'rgb(240, 161, 42)',
+        //'rgb(194, 16, 16)'
+      
     type: 'bar',
     data: {
-      labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
-        'Jul', 'Agto', 'Sept', 'Oct', 'Nov', 'Dic'],
+      labels: ['Lundi', 'Mardi', 'Mercedi', 'Juedi', 
+        'Vendredi', 'Samedi', 'Dimanche'],
       datasets: [{
-        label: 'My First Dataset',
-        data: [2, 3, 4, 4, 7, 3, 9, 3, 1, 11, 7, 3],
+        label: 'Mai, Semaine 1',        
+        data: [7,5,3,2,6,11,2],
         backgroundColor: [
-          'rgb(187, 33, 79)',
-          'rgb(138, 17, 108)',
-          'rgb(74, 5, 92)',
-          'rgb(8, 51, 129)',
-          'rgb(20, 192, 155)',
-          'rgb(19, 138, 35)',
-          'rgb(83, 226, 47)',
-          'rgb(245, 255, 105)',
-          'rgb(247, 215, 37)',
-          'rgb(240, 161, 42)',
-          'rgb(202, 102, 9)',
-          'rgb(194, 16, 16)'
-        ],
-        hoverOffset: 4
+          'rgb(20, 192, 155)'
+        ],            
+        hoverOffset: 10
+      },
+      {
+        label: 'Mai, Semaine 2',        
+        data: [5,8,9,11,12,5,3],
+        backgroundColor: [
+          'rgb(19, 138, 35)'
+        ],            
+        hoverOffset: 10
+      },
+      {
+        label: 'Mai, Semaine 3',        
+        data: [9,6,4,3,4,9,12],
+        backgroundColor: [
+          'rgb(240, 161, 42)'
+        ],            
+        hoverOffset: 10
       }]
     }
   };
