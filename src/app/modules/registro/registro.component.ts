c4e93004b55720861fc7fe7259da405c166e9371
floresrf <areneflores10@gmail.com>
2025-04-04 09:39:57 -0700

    Registro Funcional

--- src/app/modules/registro/registro.component.ts
+++ src/app/modules/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RegistroService } from '../../services/registro.service';
@@ -8,6 +9,16 @@ import { RegistroService } from '../../services/registro.service';
   selector: 'app-registro',
   standalone: true,
   imports: [CommonModule, FormsModule],
+=======
+import { UsuarioService } from '../../Service/usuario.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Usuario } from '../api';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-registro',
+  imports: [CommonModule, ReactiveFormsModule],
+>>>>>>> 19e12fe (Registro Funcional)
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
@@ -17,6 +28,7 @@ export class RegistroComponent {
   password: string = '';
   confirmPassword: string = '';
 
+<<<<<<< HEAD
   constructor(private router: Router, private registroService: RegistroService) {}
 
   registrarUsuario() {
@@ -43,6 +55,67 @@ export class RegistroComponent {
       }
     });
   }
+=======
+export class RegistroComponent implements OnInit {
+  
+  @Input('id') _id!: number; 
+  private usuarioService = inject(UsuarioService);
+  private router = inject(Router);
+  public formBuild = inject(FormBuilder);
+
+  public formUsuario : FormGroup;
+
+  
+  constructor() {
+    this.formUsuario = this.formBuild.group({
+      nombre: ['', Validators.required],   
+      correo: ['', [Validators.required, Validators.email]],   
+      contraseña: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  guardar(){
+     const objeto : Usuario={
+       _id : this._id,
+       nombre : this.formUsuario.value.nombre,
+       correo : this.formUsuario.value.correo,
+       contraseña : this.formUsuario.value.contraseña ,
+     }
+     if(this._id == null){
+       this.usuarioService.crear(objeto).subscribe({
+         next:(data)=>{
+           if(data.isRuccess){
+             this.router.navigate(['/login'])
+           }
+           else{
+            this.router.navigate(['/login'])
+           }
+         },
+         error:(err)=>{
+           console.log(err.message)
+         }
+       })
+     }
+     else{
+       this.usuarioService.edittar(objeto).subscribe({
+         next:(data)=>{
+           if(data.isRuccess){
+             this.router.navigate(['/login'])
+           }
+           else{
+             alert("Error al editar")
+           }
+         },
+         error:(err)=>{
+           console.log(err.message)
+         }
+       })
+     }
+   }
+>>>>>>> 19e12fe (Registro Funcional)
 
   RedirecLogin() {
     this.router.navigate(['/login']);
