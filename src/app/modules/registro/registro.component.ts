1ff07fc7c44c1e715ac083773f6077296d5fee53
floresrf <areneflores10@gmail.com>
2025-04-04 09:53:36 -0700

    correccion registro

--- src/app/modules/registro/registro.component.ts
+++ src/app/modules/registro/registro.component.ts
@@ -1,15 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-<<<<<<< HEAD
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { RegistroService } from '../../services/registro.service';
-
-@Component({
-  selector: 'app-registro',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
-=======
 import { UsuarioService } from '../../Service/usuario.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Usuario } from '../api';
@@ -18,44 +8,10 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-registro',
   imports: [CommonModule, ReactiveFormsModule],
->>>>>>> 19e12fe (Registro Funcional)
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
-  nombre: string = '';
-  email: string = '';
-  password: string = '';
-  confirmPassword: string = '';
 
-<<<<<<< HEAD
-  constructor(private router: Router, private registroService: RegistroService) {}
-
-  registrarUsuario() {
-    if (this.password !== this.confirmPassword) {
-      alert('Las contraseñas no coinciden');
-      return;
-    }
-
-    const nuevoRegistro = {
-      nombre: this.nombre,
-      email: this.email,
-      password: this.password
-    };
-
-    this.registroService.createRegistro(nuevoRegistro).subscribe({
-      next: (response) => {
-        console.log('Registro exitoso', response);
-        this.RedirecMenu();
-      },
-      error: (error) => {
-        console.log(nuevoRegistro)
-        console.error('Error al registrar:', error);
-        alert('Hubo un error al registrar. Revisa los datos o la conexión.');
-      }
-    });
-  }
-=======
 export class RegistroComponent implements OnInit {
   
   @Input('id') _id!: number; 
@@ -115,7 +71,7 @@ export class RegistroComponent implements OnInit {
        })
      }
    }
->>>>>>> 19e12fe (Registro Funcional)
+
 
   RedirecLogin() {
     this.router.navigate(['/login']);
